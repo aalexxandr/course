1ad382ad542a2deaab3126d11967991ba293983e
alexandr <alexander.mischenko@bask.ws>
2023-10-02 18:28:44 +0500

    fix button test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,15 +1,14 @@
-import { t } from 'i18next';
 import { Button, EButtonTheme } from '.';
 import { render, screen } from '@testing-library/react';
 
 describe('Button tests', () => {
 	test('should be Button in the document', () => {
-		render(<Button>{t('test')}</Button>);
+		render(<Button>test</Button>);
 		expect(screen.getByText('test')).toBeInTheDocument();
 	});
 
 	test('should be clear class in button', () => {
-		render(<Button theme={EButtonTheme.CLEAR}>{t('test')}</Button>);
+		render(<Button theme={EButtonTheme.CLEAR}>test</Button>);
 		expect(screen.getByText('test')).toHaveClass('clear');
 	});
 });
