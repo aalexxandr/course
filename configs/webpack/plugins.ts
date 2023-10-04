ef329cf2c7e97a747b912b22d7f3649739739017
alexandr <alexander.mischenko@bask.ws>
2023-10-04 17:16:06 +0500

    add BundleAnalyzerPlugin only for dev fix pipeline step

--- configs/webpack/plugins.ts
+++ configs/webpack/plugins.ts
@@ -7,22 +7,28 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 const webpackPlugins = ({
 	paths,
 	isDev,
-}: IBuildOptions): Array<WebpackPluginInstance> => [
-	new ProgressPlugin(),
-	new HTMLWebpackPlugin({
-		template: paths.template,
-	}),
-	new MiniCssExtractPlugin({
-		filename: 'css/[name].[contenthash:5].css',
-		chunkFilename: 'css/[name].[contenthash:5].css',
-	}),
-	new DefinePlugin({
-		__IS_DEV__: JSON.stringify(isDev),
-	}),
+}: IBuildOptions): Array<WebpackPluginInstance> => {
+	const plugins = [
+		new ProgressPlugin(),
+		new HTMLWebpackPlugin({
+			template: paths.template,
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:5].css',
+			chunkFilename: 'css/[name].[contenthash:5].css',
+		}),
+		new DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+		})
+	]
 
-	new BundleAnalyzerPlugin({
+	if (isDev) {
+		plugins.push(new BundleAnalyzerPlugin({
 		openAnalyzer: false,
-	}),
-];
+	}))
+	}
+
+	return plugins
+};
 
 export default webpackPlugins;
