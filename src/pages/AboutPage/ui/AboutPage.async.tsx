c7a70d0b95e8555ef48bac62e4244785e427fd45
alexandr <alexander.mischenko@bask.ws>
2023-10-04 17:38:08 +0500

    fix lint error

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,6 +1,7 @@
 import { FC, lazy } from 'react';
 
 export const AboutPageAsync: FC = lazy(() => new Promise((resolve) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
