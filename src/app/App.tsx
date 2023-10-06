9bfe4c837c5171f757715268ac416ea71cebcbd5
alexandr <alexander.mischenko@bask.ws>
2023-10-06 14:14:21 +0500

    Add: Modal component Some eslint changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,29 @@ import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/Router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useState } from 'react';
+import Modal from 'shared/ui/Modal';
 
 const App: FC = () => {
 	const { theme } = useTheme();
-
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const onModalClose = () => {
+		setIsModalOpen(false);
+	};
 	return (
 		<div className={`app ${theme}`}>
 			<Suspense fallback='loading...'>
 				<Navbar />
+				<button
+					onClick={() => {
+						setIsModalOpen(true);
+					}}
+				>
+					TOGGLE
+				</button>
+				<Modal isOpen={isModalOpen} onClose={onModalClose}>
+					<div>123123123</div>
+				</Modal>
 				<div className='content'>
 					<Sidebar />
 					<AppRouter />
---
+++ src/shared/ui/Modal/index.tsx
@@ -0,0 +1,78 @@
+import {
+	FC,
+	PropsWithChildren,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
+import cls from './style.module.scss';
+import { addProperties } from 'shared/lib/addProperties';
+import { MouseEvent } from 'react';
+
+interface IModal extends PropsWithChildren {
+	className?: string;
+	isOpen: boolean;
+	onClose: () => void;
+}
+
+const stopPropagation = (event: MouseEvent<HTMLDivElement>): void => {
+	event.stopPropagation();
+};
+
+const CLOSING_DELAY = 100;
+
+const Modal: FC<IModal> = ({
+	children,
+	className,
+	isOpen,
+	onClose,
+}: IModal): JSX.Element => {
+	const [isClosing, setIsClosing] = useState(false);
+	const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+	const onCloseWithTimer = useCallback(() => {
+		setIsClosing(true);
+		timerRef.current = setTimeout(() => {
+			onClose();
+			setIsClosing(false);
+		}, CLOSING_DELAY);
+	}, [timerRef, onClose]);
+
+	const onKeyDown = useCallback(
+		(e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				onCloseWithTimer();
+			}
+		},
+		[onCloseWithTimer]
+	);
+
+	useEffect(() => {
+		if (isOpen) {
+			window.addEventListener('keydown', onKeyDown);
+		}
+
+		return () => {
+			clearTimeout(timerRef.current);
+			window.removeEventListener('keydown', onKeyDown);
+		};
+	}, [isOpen, onKeyDown]);
+
+	return (
+		<div
+			className={addProperties([cls.Modal, className], {
+				[cls.open]: isOpen,
+				[cls.closing]: isClosing,
+			})}
+		>
+			<div className={cls.overlay} onClick={onCloseWithTimer}>
+				<div className={cls.content} onClick={stopPropagation}>
+					{children}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
